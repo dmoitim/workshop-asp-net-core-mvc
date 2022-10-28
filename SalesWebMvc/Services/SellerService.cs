4a3612a234bca54a38c8d6a27e7755f50d5663a0
Devair Luiz Moitim <dmoitim@empro.com.br>
2022-10-28 09:56:00 -0300

    Foreign key not null (referential integrity)

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -20,6 +20,7 @@ namespace SalesWebMvc.Services
 
         public void Insert(Seller seller)
         {
+            seller.Department = _context.Department.First(); // Only for test
             _context.Add(seller);
             _context.SaveChanges();
         }
